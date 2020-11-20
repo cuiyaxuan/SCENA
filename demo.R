d5cefc30d8dc0778bfd67f0a2bc47a0babe9e264
cuiyaxuan <35548337+cuiyaxuan@users.noreply.github.com>
2020-11-20 12:11:20 +0800

    Update demo.R

--- demo.R
+++ demo.R
@@ -4,7 +4,7 @@ install.packages(parallel)
 install.packages(SNFtool)
 install.packages(apcluster)
 install.packages(mclust)
-library(scLAEC1)
+library(SCENAcpu)
 #Import the dataset Express=read.table("E:/Biase3celltypes.txt",header = T, comment.char='!',stringsAsFactors = FALSE,quote = "",sep='\t')
 Express=Express[,-1]
 len=ncol(Express)
